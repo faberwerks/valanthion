b7d3332c468cfbd5e93d827838179d1fd6aa29b4
Phobez <abiaph@live.com>
2018-10-25 21:45:57 +0700

    Added Weapon Data Fetching
    Weapons receive their stats from the GetGameData script (attack damage, range, speed).
    By: nalinai

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,4 +7,39 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
 
+    GameManager GameManager;
+    GetGameData GameData;
+
+    string itemId = "002";
+
+    int attack;
+    int range;
+    int attackSpeed;
+
+    void Awake()
+    {
+        GameManager = GameObject.FindObjectOfType<GameManager>();
+        GameData = GameManager.GetComponent<GetGameData>();
+    }
+
+    void Start()
+    {
+        foreach (string[] currString in GameData.itemList)
+        {
+            if(string.Equals(itemId,currString[0]))
+            {
+                attack = int.Parse(currString[2]);
+                range = int.Parse(currString[3]);
+                attackSpeed = int.Parse(currString[4]);
+
+                Debug.Log(currString[0]);
+                Debug.Log(currString[1]);
+                Debug.Log(attack);
+                Debug.Log(range);
+                Debug.Log(attackSpeed);
+            }
+
+        }
+    } 
+
 }
