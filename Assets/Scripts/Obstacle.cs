31eb855855b7303f50ed2d16bd9c56b3c9ad5ee3
nalinai <samueledselfernandez@gmail.com>
2018-11-19 20:39:16 +0700

    Obstacle added
    add obstacle

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if(c.collider.tag == "Player")
+        {
+            c.transform.GetComponent<Player>().TakeDamage(20.0f);
+            Destroy(gameObject);
+        }
+    }
+
+}
