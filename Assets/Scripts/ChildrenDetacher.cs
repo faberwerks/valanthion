6d545556ad4fb1b1bed52dffbcc98c2e136eac86
Phobez <abiaph@live.com>
2018-12-18 10:25:48 +0700

    Created Stage Setup Prefab

---
+++ Assets/Scripts/ChildrenDetacher.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildrenDetacher : MonoBehaviour {
+
+    private void Awake()
+    {
+        GetComponent<Transform>().DetachChildren();
+        Destroy(gameObject);
+    }
+}
