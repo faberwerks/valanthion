230d6053a7a05c0fe5e331d080d87159c261f8f3
zalfornt <44518346+zalfornt@users.noreply.github.com>
2019-03-15 02:58:35 +0700

    Scenes added
    - Game Menu Scene added
    - Chapter Selection Scene added

---
+++ Assets/Scripts/Scenes/Game Menu/GameMenuButtonScripts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenuButtonScripts : MonoBehaviour {
+
+	public void ToggleMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ToggleContinue()
+    {
+        //Need to get latest level from Save File
+        //SceneManager.LoadScene("");
+    }
+
+    public void ToggleChapterSelection()
+    {
+        SceneManager.LoadScene("ChapterSelection");
+    }
+
+    public void ToggleCharacterSetting()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+    public void ToggleOptions()
+    {
+        //SceneManager.LoadScene("");
+    }
+
+
+}
