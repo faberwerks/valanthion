36beb80d63a8a9733477c435f82e8c1e308b7602
Phobez <abiaph@live.com>
2019-03-15 21:02:40 +0700

    Created Prologue and adjusted Title Screen

---
+++ Assets/Scripts/UI/Temporary/CutsceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour {
+
+    public GameObject[] cutscenes = new GameObject[5];
+
+    public float interval = 5f;
+
+	// Use this for initialization
+	private void Start () {
+        StartCoroutine(CStartCutscenes());
+	}
+	
+	private IEnumerator CStartCutscenes()
+    {
+        yield return new WaitForSeconds(interval);
+        cutscenes[0].SetActive(false);
+        cutscenes[1].SetActive(true);
+        yield return new WaitForSeconds(interval);
+        cutscenes[1].SetActive(false);
+        cutscenes[2].SetActive(true);
+        yield return new WaitForSeconds(interval);
+        cutscenes[2].SetActive(false);
+        cutscenes[3].SetActive(true);
+        yield return new WaitForSeconds(interval);
+        cutscenes[3].SetActive(false);
+        cutscenes[4].SetActive(true);
+        yield return new WaitForSeconds(interval);
+        SceneManager.LoadScene("DemoLevel2");
+    }
+}
