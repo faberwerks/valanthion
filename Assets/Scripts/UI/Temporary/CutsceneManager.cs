bd276c0248045ca13993db178213a518e0e1d5ff
Phobez <abiaph@live.com>
2019-03-16 21:51:55 +0700

    Updated interval times between cutscenes

--- Assets/Scripts/UI/Temporary/CutsceneManager.cs
+++ Assets/Scripts/UI/Temporary/CutsceneManager.cs
@@ -7,7 +7,7 @@ public class CutsceneManager : MonoBehaviour {
 
     public GameObject[] cutscenes = new GameObject[5];
 
-    public float interval = 5f;
+    public float[] intervals = new float[5];
 
 	// Use this for initialization
 	private void Start () {
@@ -16,19 +16,19 @@ public class CutsceneManager : MonoBehaviour {
 	
 	private IEnumerator CStartCutscenes()
     {
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(intervals[0]);
         cutscenes[0].SetActive(false);
         cutscenes[1].SetActive(true);
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(intervals[1]);
         cutscenes[1].SetActive(false);
         cutscenes[2].SetActive(true);
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(intervals[2]);
         cutscenes[2].SetActive(false);
         cutscenes[3].SetActive(true);
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(intervals[3]);
         cutscenes[3].SetActive(false);
         cutscenes[4].SetActive(true);
-        yield return new WaitForSeconds(interval);
+        yield return new WaitForSeconds(intervals[4]);
         SceneManager.LoadScene("DemoLevel2");
     }
 }
