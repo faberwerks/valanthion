26d2f85e9e50697d94e8ba21cd85035158a27e31
Phobez <abiaph@live.com>
2019-03-20 00:17:33 +0700

    Tidied up files and finished chapter selection screen

---
+++ Assets/ChapterSelection.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChapterSelection : MonoBehaviour {
+
+    public Button[] stageButtons = new Button[6];
+
+    public int selectedStage = 0;
+
+	// Use this for initialization
+	private void Start () {
+		for (int i = 0; i < Game.current.latestStage; i++)
+        {
+            stageButtons[i].interactable = true;
+        }
+	}
+	
+    // set currently selected stage
+	public void SetSelectedStage(int stageIndex)
+    {
+        selectedStage = stageIndex;
+    }
+
+    // load selected stage
+    public void LoadSelectedStage()
+    {
+        SceneManager.LoadScene(selectedStage);
+    }
+}
