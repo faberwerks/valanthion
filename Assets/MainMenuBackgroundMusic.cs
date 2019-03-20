7d8eabe7d199be825eb1539f23bb0c2586de2180
Phobez <abiaph@live.com>
2019-03-20 22:08:10 +0700

    Adjusted some things

---
+++ Assets/MainMenuBackgroundMusic.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuBackgroundMusic : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+    private int currentSceneIndex;
+
+	// Use this for initialization
+	private void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if ((currentSceneIndex > 0 && currentSceneIndex < 7) || currentSceneIndex == 9)
+        {
+            audioSource.Pause();
+        }
+        else
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.time = 0;
+                audioSource.Play();
+            }
+        }
+	}
+}
