cba620418e690a3b0d407677b5323b182b2a36b7
Phobez <abiaph@live.com>
2019-03-20 13:01:06 +0700

    Added fade in/out for cutscenes and option to skip cutscenes

--- Assets/Scripts/UI/Temporary/CutsceneManager.cs
+++ Assets/Scripts/UI/Temporary/CutsceneManager.cs
@@ -13,22 +13,64 @@ public class CutsceneManager : MonoBehaviour {
 	private void Start () {
         StartCoroutine(CStartCutscenes());
 	}
-	
-	private IEnumerator CStartCutscenes()
+
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StopAllCoroutines();
+            SceneManager.LoadScene("DemoLevel2");
+        }
+    }
+
+    private IEnumerator CStartCutscenes()
+    {
+        StartCoroutine(FadeIn(0));
         yield return new WaitForSeconds(intervals[0]);
-        cutscenes[0].SetActive(false);
-        cutscenes[1].SetActive(true);
+        StartCoroutine(FadeOut(0));
+
         yield return new WaitForSeconds(intervals[1]);
-        cutscenes[1].SetActive(false);
-        cutscenes[2].SetActive(true);
+        StartCoroutine(FadeOut(1));
+
         yield return new WaitForSeconds(intervals[2]);
-        cutscenes[2].SetActive(false);
-        cutscenes[3].SetActive(true);
+        StartCoroutine(FadeOut(2));
+
         yield return new WaitForSeconds(intervals[3]);
-        cutscenes[3].SetActive(false);
-        cutscenes[4].SetActive(true);
+        StartCoroutine(FadeOut(3));
+
         yield return new WaitForSeconds(intervals[4]);
+        StartCoroutine(FadeOut(4));
         SceneManager.LoadScene("DemoLevel2");
+    }
+
+    // a method to fade in image
+    private IEnumerator FadeIn(int imageIndex)
+    {
+        SpriteRenderer _sprRend = cutscenes[imageIndex].GetComponent<SpriteRenderer>();
+
+        for (float i = 0; i <= 1; i += Time.deltaTime / 2)
+        {
+            _sprRend.color = new Color(i, i, i, 1);
+            yield return null;
+        }
+    }
+
+    // a method to fade out image
+    private IEnumerator FadeOut(int imageIndex)
+    {
+        SpriteRenderer _sprRend = cutscenes[imageIndex].GetComponent<SpriteRenderer>();
+
+        for (float i = 1; i >= 0; i -= Time.deltaTime / 2)
+        {
+            _sprRend.color = new Color(i, i, i, 1);
+            yield return null;
+        }
+
+        if (imageIndex < 4)
+        {
+            cutscenes[imageIndex].SetActive(false);
+            cutscenes[imageIndex + 1].SetActive(true);
+            StartCoroutine(FadeIn(imageIndex + 1));
+        }
     }
 }
