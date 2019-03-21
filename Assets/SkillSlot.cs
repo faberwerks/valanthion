8f6317dfcf447f7eb2b85d0ae17eadd416f159a3
Phobez <abiaph@live.com>
2019-03-21 07:38:40 +0700

    Added skill slots to new canvas prefab

---
+++ Assets/SkillSlot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillSlot : MonoBehaviour {
+
+    public Image[] skillIcons = new Image[6];
+
+    public KeyCode skillSlotKey;
+
+	// Use this for initialization
+	private void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		
+	}
+}
