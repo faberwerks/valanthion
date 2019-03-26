e8b45a586a070fbd95e31c7f9ce9aa5d0fe8f0f7
Phobez <abiaph@live.com>
2019-03-26 21:31:49 +0700

    Created stage 2 camera limiter

---
+++ Assets/Scripts/UI/CameraConstraint.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraConstraint : MonoBehaviour{
+
+    private Vector3 camPos;
+
+    public float topBorder;
+    public float rightBorder;
+    public float bottomBorder;
+    public float leftBorder;
+
+    private float camHeight;
+    private float camWidth;
+
+    private void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+
+    public Vector3 Constraint(Vector3 currPos)
+    {
+        if (currPos.y + camHeight > topBorder)
+        {
+            currPos.y = topBorder - camHeight;
+        }
+        if (currPos.y - camHeight < bottomBorder)
+        {
+            currPos.y = bottomBorder + camHeight;
+        }
+        if (currPos.x + camWidth > rightBorder)
+        {
+            currPos.x = rightBorder - camWidth;
+        }
+        if (currPos.x - camWidth < leftBorder)
+        {
+            currPos.x = leftBorder + camWidth;
+        }
+        return (currPos);
+    }   
+}
--- Assets/Scripts/UI/CameraMovement.cs
+++ Assets/Scripts/UI/CameraMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraMovement : MonoBehaviour {
 
@@ -9,10 +10,23 @@ public class CameraMovement : MonoBehaviour {
     private Vector3 currPos;
 
     public float zOffset = -10.0f;
+    public float topBorder;
+    public float rightBorder;
+    public float bottomBorder;
+    public float leftBorder;
+
+    private float camHeight;
+    private float camWidth;
+
+    private int stageNumber;
 
     private void Awake()
     {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+
         player = GameObject.FindGameObjectWithTag("Player");
+        stageNumber = SceneManager.GetActiveScene().buildIndex;
     }
 
     void LateUpdate ()
@@ -20,6 +34,13 @@ public class CameraMovement : MonoBehaviour {
         currPos = player.transform.position;
         currPos.z += zOffset;
 
+        if(stageNumber == 2)
+        {
+            var clampedX = Mathf.Clamp(player.transform.position.x, leftBorder + camWidth, rightBorder - camWidth);
+            var clampedY = Mathf.Clamp(player.transform.position.y , bottomBorder + camHeight, topBorder - camHeight);
+            currPos = new Vector3(clampedX, clampedY, currPos.z);
+        }
+            
         transform.position = currPos;
 	}
 
