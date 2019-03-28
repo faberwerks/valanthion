eee41790af7f52a9aa790ecf8fd0d7bf938db467
zalfornt <44518346+zalfornt@users.noreply.github.com>
2019-03-29 00:33:12 +0700

    Stage 1 Tutorial Added
    Tutorial follows player the first time player start stage 1

---
+++ Assets/Scripts/TutorialSteps.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialSteps : MonoBehaviour {
+
+    public string tutorialText;
+
+    public int step;
+	
+    public void ChangeTutorialText(GameObject player, GameObject tutorCanvas)
+    {
+        player.GetComponent<TutorialTextController>().latestStep = step;
+        tutorCanvas.GetComponent<Text>().text = tutorialText;
+    }
+
+}
