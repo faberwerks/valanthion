f769bbf851f3f389152f1e3253f1b8d5bd13b1d5
zalfornt <44518346+zalfornt@users.noreply.github.com>
2019-03-29 15:07:00 +0700

    Final fixes before build

--- Assets/Scripts/Bosses/MiniBoss.cs
+++ Assets/Scripts/Bosses/MiniBoss.cs
@@ -74,7 +74,7 @@ public class MiniBoss : Entity {
     public void TakeDamage(int atk)
     {
         health -= atk;
-
+        audioSource.Play();
         StartCoroutine(CTimeColorChange());
     }
 
