5ea937f83160c027c7befa94a283a58ce170e35f
zalfornt <44518346+zalfornt@users.noreply.github.com>
2019-03-29 12:54:23 +0700

    Skill Button changes
    Skill Button  now intercatable when not unlockable
    changes to stage 1
    changed health of Barrier prefab

---
+++ Assets/Scripts/SkillTreeButtonColor.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillTreeButtonColor : MonoBehaviour {
+
+    private Button buttonComponent;
+
+    private ColorBlock buttonCB;
+
+    public ColorBlock unlockable;
+    public ColorBlock notUnlockable;
+
+    public int prevId;
+    public int prevId2;
+
+    public bool hasTwoRequirements;
+
+    // Use this for initialization
+    private void Start()
+    {
+        buttonComponent = GetComponent<Button>();
+        buttonCB = buttonComponent.colors;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!hasTwoRequirements)
+        {
+            if (Game.current.skills[prevId] != 0)
+            {
+                buttonComponent.colors = unlockable;
+            }
+            else
+            {
+                buttonComponent.colors = notUnlockable;
+            }
+        }
+        else
+        {
+            if (Game.current.skills[prevId] != 0 || Game.current.skills[prevId2] != 0)
+            {
+                buttonComponent.colors = unlockable; ;
+            }
+            else
+            {
+                buttonComponent.colors = notUnlockable;
+            }
+        }
+    }
+}
